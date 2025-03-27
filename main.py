75589b01919c7768bd468c7dd50c087eb6cc9d01
MadhavanK24 <shikeitsrong12@gmail.com>
2025-03-27 20:37:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import importlib 
+import audio_and_silence 
+
+importlib.reload(audio_and_silence) 
+
+from audio_and_silence import AudioProcessor 
+
+audio = AudioProcessor("<input file name>", format="<input format>") 
+audio.process_audio(min_silence_len=<custom length>, threshold=-60, 
+                    output_folder="outputs") 
